b9857e7d88dcc468a92fe2fcbc8460a3fd9ded6c
Diegames364 <diegames364@gmail.com>
2026-02-05 08:39:45 -0500

    Arreglo en archivo build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); import os; username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); password=os.environ.get('Hector'); User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)" | python manage.py shell
+python manage.py shell << 'EOF'
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superusuario creado")
+    else:
+        print("Superusuario ya existe")
+else:
+    print("Variables de entorno no definidas")
+EOF
